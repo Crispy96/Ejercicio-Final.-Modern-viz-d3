f250ce74eb4e418080e8c20136c3ba4d832f333a
Cristina Rodríguez <crroga96@gmail.com>
2022-01-09 16:34:48 +0100

    Representación chicas

--- main.js
+++ main.js
@@ -13,20 +13,42 @@ const age = function(year) { return year - diCaprioBirthYear}
 const today = new Date().getFullYear()
 const ageToday = age(today)
 
-const svg = d3.select("#chart").append("svg").attr("width", width).attr("height", height)
+const svg = d3.select("#chart").append("svg").attr("id","svg").attr("width", width).attr("height", height)
 const elementGroup = svg.append("g").attr("id", "elementGroup")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-var x = d3.scaleBand().range([0, width - margin.left - margin.right])
-var y = d3.scaleTime().range([height - margin.top - margin.bottom, 0])
+var x = d3.scaleBand().range([0, width - margin.left - margin.right]).padding(0.2)
+var y = d3.scaleLinear().range([height - margin.top - margin.bottom, 0])
 
 const axisGroup = svg.append("g").attr("id", "axisGroup")
 const xGroup = axisGroup.append("g").attr("id", "xGroup")
+    .attr("transform", `translate(${margin.left}, ${height- margin.bottom})`)
 const yGroup = axisGroup.append("g").attr("id", "yGroup")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)   
 
-const axisX = d3.axisBottom().scale(x)
-const axisY = d3.axisLeft().scale(y)
+var axisX = d3.axisBottom().scale(x)
+var axisY = d3.axisLeft().scale(y)
+
+const formatDate = d3.timeParse("%Y")
 
 d3.csv("data.csv").then(data =>{
-    console.log()
-} )
+    data.map(d =>{
+        d.age = +d.age
+        d.year = +d.year
+    })
+    x.domain(data.map(d=>d.year))
+    y.domain([0, ageToday ])
+    
+    xGroup.call(axisX)
+    yGroup.call(axisY)
 
+    elementGroup.selectAll("rect").data(data).join("rect")
+        .attr("x", d => x(d.year))
+        .attr("y", d => y(d.age))
+        .attr("height", d => height - margin.top - margin.bottom - y(d.age))
+        .attr("width", x.bandwidth())
+    
+        console.log(data)
+
+
+    } )
